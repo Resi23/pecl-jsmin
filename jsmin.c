665e14835fd7ca53318f1c9226552aa1b5c6e5b8
Resi23 <x-resistant-x@gmx.de>
2021-02-08 17:57:47 +0100

    Update jsmin.c
    php8 fix

--- jsmin.c
+++ jsmin.c
@@ -34,7 +34,7 @@ SOFTWARE.
 */
 
 static jsmin_obj*
-new_jsmin_obj(char *javascript TSRMLS_DC)
+new_jsmin_obj(char *javascript)
 {
 	jsmin_obj *jmo	= ecalloc(1, sizeof(jsmin_obj));
 	jmo->javascript = javascript;
@@ -48,7 +48,7 @@ new_jsmin_obj(char *javascript TSRMLS_DC)
 /* free_jsmin_obj -- frees up memory on struct
 */
 void
-free_jsmin_obj(jsmin_obj *jmo TSRMLS_DC)
+free_jsmin_obj(jsmin_obj *jmo)
 {
 	smart_string_free(&jmo->buffer);
 	efree(jmo);
@@ -250,7 +250,7 @@ jsmin_action(int d, jsmin_obj *jmo)
 */
 
 jsmin_obj*
-jsmin(char *javascript TSRMLS_DC)
+jsmin(char *javascript)
 {
 	jsmin_obj *jmo = new_jsmin_obj(javascript TSRMLS_CC);
 
